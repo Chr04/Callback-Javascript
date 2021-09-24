9e8368866965c3f7ad8835a66bb9d732a92f6de4
Chr04 <christiandc99@gmail.com>
2021-09-24 17:59:01 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function criaBotao(text, callback) {
+    const body = document.querySelector("body");
+    const botao = document.createElement("button");
+
+    callback(botao);
+
+    body.insertAdjacentElement("beforeend", botao);
+    botao.textContent = text;
+}
+
+criaBotao("Entrar", (botao) => {
+    botao.style.cssText = `
+        font-size: 20px;
+        color: purple;
+        padding: 30px;
+    `;
+});
+
+criaBotao("Sair", (botao) => {
+    botao.style.cssText = `
+        color: brown;
+        padding: 60px;
+        border-radius: 20px;
+        margin-left: 30px;
+    `;
+})
